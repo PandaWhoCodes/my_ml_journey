73e73007a60e235bc16f38b06c480c550560e0f6
Ashish <ufoundashish@gmail.com>
2018-05-28 00:11:11 +0530

    PCA

--- PCA/pca2.py
+++ PCA/pca2.py
@@ -5,7 +5,6 @@ from numpy.linalg import eig
 import pandas as pd
 
 # define a matrix
-A = array([[1, 2], [3, 4], [5, 6]])
 df = pd.read_csv("dataset.csv", skiprows=1, names=['Sample', 'Type', 'PW', 'PL', 'SW', 'SL'])
 # Dropping the sample number from dataframe
 df.drop(['Sample'], axis=1)
@@ -13,17 +12,31 @@ features = ['Type', 'PW', 'PL', 'SW', 'SL']
 A = df
 # calculate the mean of each column
 M = mean(A.T, axis=1)
-print(M)
+# print(M)
 # center columns by subtracting column means
 C = A - M
-print(C)
+# print(C)
 # calculate covariance matrix of centered matrix
 V = cov(C.T)
-print(V)
+# print(V)
 # eigen decomposition of covariance matrix
 values, vectors = eig(V)
-print(vectors)
-print(values)
+# print(vectors)
+# print(values)
 # project data
 P = vectors.T.dot(C.T)
-print(P.T)
+# print(P.T)
+from sklearn.decomposition import PCA
+
+# define a matrix
+# print(A)
+# create the PCA instance
+pca = PCA(1)
+# fit on data
+pca.fit(A)
+# access values and vectors
+# print(pca.components_)
+# print(pca.explained_variance_)
+# transform data
+B = pca.transform(A)
+print(B)
